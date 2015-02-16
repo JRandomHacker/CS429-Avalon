bc7f2cd6dae907bcc26920b9bd36cc8a5fb6c34e
Ryan Kerr <rkerr2@illinois.edu>
2015-02-16 16:34:48 -0600

    Changed globals to be a namespace
    Added a new namespace, avalon, in globals to be more "correct"
    Required changes in the player class, since it was using things in
    global, which are now inside of a namespace.

--- src/globals.hpp
+++ src/globals.hpp
@@ -1,5 +1,6 @@
 /**
- * A header designed to hold any structs/enums/defines that should be used across all classes
+ * A file designed to hold any structs/enums/defines that should be used across all classes
+ * All values are part of namespace avalon
  *
  * @author Ryan Kerr && Justin Koehler
  * @date 2015-02-16
@@ -11,19 +12,34 @@
 
 #define DEFAULT_PORT 42917
 
-enum special_roles_t {
-    MERLIN,
-    PERCIVAL,
-    MORDRED,
-    MORGANA,
-    ASSASSIN,
-    OBERON,
-    NONE
-};
+namespace avalon {
 
-enum alignment_t {
-    GOOD,
-    EVIL
-};
+    static const int NUM_SPECIALS = 5;
+
+    /*! An enumerator for the possible special roles */
+    enum special_roles_t {
+        MERLIN,   /*! Merlin (Good) Knows evil players */
+        PERCIVAL, /*! Percival (Good) Knows of Morgana and Merlin*/
+        MORDRED,  /*! Mordred (Evil) Unknown to Merlin */
+        MORGANA,  /*! Morgana (Evil) Known to Percival (Disguises Merlin) */
+        ASSASSIN, /*! Assassin (Evil) Gets a last ditch chance to kill Merlin, making Evil win */
+        OBERON,   /*! Oberon (Evil) Unknown to other Evil players */
+        NONE      /*! None Corresponds to being a generic good/evil person */
+    };
+
+    /*! An enumerator of the possible alignments */
+    enum alignment_t {
+        GOOD, /*! Good (Wants missions to pass) */
+        EVIL /*! Evil (Wants missions to fail) */
+    };
+
+    /**
+     * A function to return the alignment of a role
+     *
+     * @param avalon::special_roles_t the special role to examine - Passing in the special role NONE is undefined
+     * @return avalon::alignment_t The alignment of the role
+     */
+    alignment_t roleAlignment( special_roles_t role );
+}
 
 #endif // _GLOBALS_HPP
