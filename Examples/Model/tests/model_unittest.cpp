2e92c376978de7b3c101a4c7a1ff60b2541527d4
mhoff <mhoff12358@gmail.com>
2015-02-17 03:38:34 -0600

    Added tests covering the model, including adding and removing subscribers, getting data through subscribers, and gracefully handling destroying the model.

--- Examples/Model/tests/model_unittest.cpp
+++ Examples/Model/tests/model_unittest.cpp
@@ -51,7 +51,85 @@ TEST_F( ModelTests, testAbleToSubscribeWithData) {
     ASSERT_TRUE(model.unsubscribe("data_name", &sub1));
     ASSERT_TRUE(model.unsubscribe("data_name", &sub2));
 
-    // Ensure the subscribers had their destruction method called
+    // Ensure the subscribers had their destruction method called and no longer
+    // have references to the model data
+
     ASSERT_TRUE(sub1.wasDestroyed);
+    ASSERT_EQ((ModelData*)NULL, sub1.getModelData());
     ASSERT_TRUE(sub2.wasDestroyed);
+    ASSERT_EQ((ModelData*)NULL, sub2.getModelData());
+}
+
+TEST_F( ModelTests, testAbleToUpdateAndRetrieveData) {
+    MockSubscriber sub1;
+    MockSubscriber sub2;
+
+    // Adds data to subscribe to
+    model.addData("data_name", 0);
+
+    // Successfully subscribe to the new data
+    ASSERT_TRUE(model.subscribe("data_name", &sub1));
+
+    // Ensure that the subscriber can access the data
+    ASSERT_EQ(0, sub1.getModelData()->getPayload());
+
+    // Updates the data and ensures that the subscriber is still accurate and
+    // that its update method was called
+    model.updateData("data_name", 1);
+    ASSERT_EQ(1, sub1.getModelData()->getPayload());
+    ASSERT_EQ(1, sub1.updatedCount);
+
+    // Adds a second subscriber
+    ASSERT_TRUE(model.subscribe("data_name", &sub2));
+    ASSERT_EQ(1, sub2.getModelData()->getPayload());
+
+    // Updates the data and ensures that both subscribers were updated
+    model.updateData("data_name", 2);
+    ASSERT_EQ(2, sub1.getModelData()->getPayload());
+    ASSERT_EQ(2, sub1.updatedCount);
+    ASSERT_EQ(2, sub2.getModelData()->getPayload());
+    ASSERT_EQ(1, sub2.updatedCount);
+
+    // Since the subscribers are being destroyed they need to be freed
+    ASSERT_TRUE(model.unsubscribe("data_name", &sub1));
+    ASSERT_TRUE(model.unsubscribe("data_name", &sub2));
+}
+
+TEST_F( ModelTests, testDeletingDataRemovesSubscribers) {
+    MockSubscriber sub1;
+    
+    // Adds data to subscribe to
+    model.addData("data_name", 0);
+
+    // Successfully subscribe to the new data
+    ASSERT_TRUE(model.subscribe("data_name", &sub1));
+
+    // Remove the data from the model
+    ASSERT_TRUE(model.removeData("data_name"));
+
+    // Ensure that the subscriber now no longer has access to the data and that
+    // its destruction method was called
+    ASSERT_EQ((ModelData*)NULL, sub1.getModelData());
+    ASSERT_TRUE(sub1.wasDestroyed);
+}
+
+TEST_F( ModelTests, testModelDestructed) {
+    MockSubscriber sub1;
+
+    // Creates a model that can be conveniently destructed on command
+    Model* destroyable_model = new Model();
+
+    // Adds data to subscribe to
+    destroyable_model->addData("data_name", 0);
+
+    // Successfully subscribe to the new data
+    ASSERT_TRUE(destroyable_model->subscribe("data_name", &sub1));
+
+    // Destroy the model
+    delete destroyable_model;
+
+    // Ensure that the subscriber now no longer has access to the data and that
+    // its destruction method was called
+    ASSERT_EQ((ModelData*)NULL, sub1.getModelData());
+    ASSERT_TRUE(sub1.wasDestroyed);
 }
