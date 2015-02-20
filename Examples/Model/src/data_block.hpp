83d2ec7d36d46a41be008d460b2e95bf5fbb5023
Mohammad Askari <mhaskari92@gmail.com>
2015-02-20 12:26:03 -0600

    Update data_block.hpp
    Added function documentation.

--- Examples/Model/src/data_block.hpp
+++ Examples/Model/src/data_block.hpp
@@ -15,14 +15,40 @@ public:
 	~DataBlock();
 	DataBlock(const DataBlock& that);
 
+	/**
+	 * Adds a subscriber so that they can be notified of any changes.
+	 * @param new_subscriber Pointer to the Subscriber object for our new subscriber.
+	 * @return true on success
+	**/
 	bool addSubscriber(Subscriber* new_subscriber);
+	/**
+	 * Releases an old subscriber.
+	 * @param old_subscriber Pointer to the Subscriber object for the old subscriber to be released.
+	 * @return true if subscriber successfully released, false if not found
+	**/
 	bool detachSubscriber(Subscriber* old_subscriber);
+	/**
+	 * Removes an old subscriber without destroying its data.
+	 * @param old_subscriber Pointer to the Subscriber object for the old subscriber to be removed.
+	 * @return true if subscriber successfully removed, false if not found
+	**/
 	bool detachSubscriberQuietly(Subscriber* old_subscriber);
+	/**
+	 * Release all subscribers.
+	**/
 	void detachAllSubscribers();
 
+	/**
+	 * Updates the DataBlcok with new data and notifies all subscribers.
+	 * @param new_data the new data to add to the DataBlock
+	**/
 	template <typename T>
 	void updateData(const T& new_data);
 
+	/**
+	 * Returns the current data, if there is any.
+	 * @return current data or null if no data exists
+	**/
 	template <typename T>
 	T* getData();
 
@@ -30,9 +56,19 @@ private:
 	ModelData* data_payload = NULL;
 	std::vector<Subscriber*> subscribers;
 	
+	/** 
+	 * Deletes all current data.
+	**/
 	void releaseData();
+	/**
+	 * Deletes data of given subscriber.
+	 * @param old_subscriber Pointer to Subscriber object whose data will be deleted
+	**/
 	void releaseSubscriber(Subscriber* old_subscriber);
 
+	/**
+	 * Notifies all subscribers that data was updated.
+	**/
 	void alertAllSubscribersToUpdate();
 };
 
