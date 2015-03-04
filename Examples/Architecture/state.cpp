475291c3b1b699f2ebbc6e717aa792c362302096
Mohammad Askari <mhaskari92@gmail.com>
2015-03-03 19:35:19 -0600

    Update state.cpp

--- Examples/Architecture/state.cpp
+++ Examples/Architecture/state.cpp
@@ -17,14 +17,19 @@ State::~State() {
 
 void State::actionHandle(Action* newAct) {
     std::string actionType = newAct->GetMessage();
+    std::string actionData;
     if (strcmp(actionType, "CreateGame") == 0)
       //send string of command args from Action obj to Server from GUI
+      actionData = (CreateGameAction*)newAct->GetArgs();
     else if (strcmp(actionType, "JoinGame") == 0)
       //send string representing IP and port of server and player information from GUI
+      actionData = (JoinGameAction*)newAct->GetServerInfo();
     else if (strcmp(actionType, "EnterLobby") == 0)
       //send game settings info for lobby from Network
+      actionData = (EnterLobbyAction*)newAct->GetSettings();
     else if (strcmp(actionType, "AddPlayer") == 0)
       //send info about new player in game from Network
+      actionData = (AddPlayerAction*)newAct->GetPlayerInfo();
     else
       //invalid action
 }
