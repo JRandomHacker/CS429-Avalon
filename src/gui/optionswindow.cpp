7b8cf8e8c0577458f329e3535b6fc98fc2efce1e
justin <jkoehle2@illinois.edu>
2015-03-03 18:33:35 -0600

    windows fork

--- src/gui/optionswindow.cpp
+++ src/gui/optionswindow.cpp
@@ -4,6 +4,10 @@
 #include <iostream>
 #include <string>
 
+#ifdef _WIN32
+	#include <windows.h>
+#endif
+
 /*
  *  Possible args:
  *  --players=x (This one is REQUIRED)
@@ -31,39 +35,77 @@ OptionsWindow::~OptionsWindow( ) {
 }
 
 void OptionsWindow::createServer() {
-    if( fork( ) == 0 ) {
-        int i = 0;
-        char* args[ MAX_ARGS + 1 ]; // +1 for the NULL pointer at the end of the array
+	#ifdef _WIN32
+		std::string execStr = "./server.exe"; 
 
         // Get the number of players
-        std::string playerStr = "--players=";
-        playerStr += std::to_string( ui->serverSectionPlayerAmount->value( ) );
-        std::cout << playerStr << std::endl;
+        execStr += " --players=";
+        execStr += std::to_string( ui->serverSectionPlayerAmount->value( ) );
 
         QList<QListWidgetItem* > roles = ui->listWidget->selectedItems();
 
-        // First argument must be the executable
-        args[ i++ ] = (char*)"../server.exe";
-        args[ i++ ] = (char*)playerStr.c_str();
-
         for ( int j = 0; j < roles.length(); j++ ) {
-            std::string dashDash = "--";
-            dashDash += roles[j]->text().toStdString();
-            std::cout << dashDash << std::endl;
-            args[ i++ ] = (char*)dashDash.c_str();
+            std::string flagStr = " --";
+            flagStr += roles[j]->text().toStdString();
+            execStr += flagStr;
         }
 
-        args[ i ] = NULL; // Must be null terminated
-        
-        // execv takes an array for the arguments
-        execv( args[ 0 ], args );
+		// Setup required structs for windows process
+		STARTUPINFO StartupInfo;
+		PROCESS_INFORMATION ProcessInfo;
+		
+		// Don't show the console in the new process
+		memset(&StartupInfo, 0, sizeof(StartupInfo));
+		StartupInfo.cb = sizeof(STARTUPINFO);
+		StartupInfo.dwFlags = STARTF_USESHOWWINDOW;
+		StartupInfo.wShowWindow = SW_HIDE;
+		
+		char* execChar = new char[ execStr.size( ) ];
+		strcpy( execChar, execStr.c_str( ) );
 
-        // If we get here, the executable crashed, so exit this process (The main process is still running)
-        // If the server exits on its own, we won't get here
-        std::cerr << "Unable to find server executable, or something terrible happened!" << std::endl;
-        exit( 1 );
-    }
-    else {
-        std::cout << "Server Created." << std::endl;
-    }
+		if (!CreateProcess( NULL, execChar, NULL, NULL, FALSE,
+			DETACHED_PROCESS, 
+			NULL, 
+			NULL,
+			&StartupInfo,
+			&ProcessInfo))
+		{
+			std::cerr << "Unable to find server executable, or something terrible happened!" << std::endl;	
+		}
+	#else 
+		if( fork( ) == 0 ) {
+	        int i = 0;
+	        char* args[ MAX_ARGS + 1 ]; // +1 for the NULL pointer at the end of the array
+	
+	        // Get the number of players
+	        std::string playerStr = "--players=";
+	        playerStr += std::to_string( ui->serverSectionPlayerAmount->value( ) );
+	        std::cout << playerStr << std::endl;
+	
+	        QList<QListWidgetItem* > roles = ui->listWidget->selectedItems();
+	
+	        // First argument must be the executable
+	        args[ i++ ] = (char*)"../server.exe";
+	        args[ i++ ] = (char*)playerStr.c_str();
+	
+	        for ( int j = 0; j < roles.length(); j++ ) {
+	            std::string dashDash = "--";
+	            dashDash += roles[j]->text().toStdString();
+	            std::cout << dashDash << std::endl;
+	            args[ i++ ] = (char*)dashDash.c_str();
+	        }
+	
+	        args[ i ] = NULL; // Must be null terminated
+	        // execv takes an array for the arguments
+	        execv( args[ 0 ], args );
+	
+	        // If we get here, the executable crashed, so exit this process (The main process is still running)
+	        // If the server exits on its own, we won't get here
+	        std::cerr << "Unable to find server executable, or something terrible happened!" << std::endl;
+	        exit( 1 );
+	    }
+	    else {
+	        std::cout << "Server Created." << std::endl;
+	    }
+    #endif
 }
