46afc32e19bf7bcaeaad2d3e4518ebf9dab3d57f
justin <jkoehle2@illinois.edu>
2015-03-04 23:09:49 -0600

    bugfix: linux port number

--- src/gui/createserverwindow.cpp
+++ src/gui/createserverwindow.cpp
@@ -79,7 +79,7 @@ void CreateServerWindow::createServer() {
 	        // First argument must be the executable
 	        args[ i++ ] = (char*)"./server.exe";
 	        args[ i++ ] = (char*)playerStr.c_str();
-            args[ i++ ] = (char*)ui->editPortNum->text().toStdString();
+            args[ i++ ] = (char*)ui->editPortNum->text().toStdString().c_str( );
           
 	        for ( int j = 0; j < roles.length(); j++ ) {
                 std::string flagStr = "--";
