ea14d7df77fe54f1bf992b00c72e4d89d682e2ac
Mohammad Askari <mhaskari92@gmail.com>
2015-03-06 16:42:29 -0600

    Tested handler, still must make GameSettings and Player and find segfault from FreezeFrontActions()

--- tests/action_unittest.cpp
+++ tests/action_unittest.cpp
@@ -1,16 +1,18 @@
 /**
- * Tests for player.cpp
- * 
- * @author Justin Koehler && Ryan Kerr
- * @date 2015-02-16
- * @file player_unittest.cpp
+ * Tests for ActionHandler.cpp && Action.cpp
+ *
+ * @author Mohammad Askari && Matt Hoffman
+ * @date 2015-03-06
+ * @file action_unittest.cpp
  */
 
 #include "ActionHandler.hpp"
 #include "Action.hpp"
+#include "CustomActions.hpp"
 #include "gtest/gtest.h"
 
 #include <string>
+#include <semaphore.h>
 
 class ActionTests : public ::testing::Test {
 public:
@@ -20,3 +22,36 @@ TEST_F( ActionTests, testActionClassesExist) {
     ActionHandler ah(NULL);
     Action a("invalidActionType");
 }
+
+TEST_F( ActionTests, testAddingActions) {
+    sem_t mut;
+    sem_init(&mut, 1, 1);
+    ActionHandler ahandler(&mut);
+    Action act("CreateGame");
+    ASSERT_TRUE(act.GetMessage() == "CreateGame");
+    Action act2("JoinGame");
+    ASSERT_TRUE(act2.GetMessage() == "JoinGame");
+    ahandler.AddAction(&act);
+    ahandler.AddAction(&act2);
+    ASSERT_EQ(2,2);
+    auto p = ahandler.FreezeFrontActions();
+    ASSERT_EQ(std::get<0>(p), 2);
+    ahandler.ReleaseFrozenActions();
+    ASSERT_EQ(2,2);
+    p = ahandler.FreezeFrontActions();
+    ASSERT_EQ(std::get<0>(p), 0);
+}
+
+/*
+TEST_F( ActionTests, testCustomActions) {
+    GameSettings settings;
+    GameSettingsAction gsact(&settings);
+    ASSERT_TRUE(gsact.GetMessage() == "GameSettings");
+    ASSERT_EQ(gsact.getSettings(), &settings);
+    Player play;
+    AddPlayerAction plact(2, &play);
+    ASSERT_TRUE(plact.GetMessage() == "AddPlayer");
+    ASSERT_EQ(plact.getPlayerNumber(), 2);
+    ASSERT_EQ(plact.getPlayerInfo(), &play);
+}
+*/
