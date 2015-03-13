b7686eae3060e1ed7d374d93c7988c2a0095696c
Ryan Kerr <rkerr2@illinois.edu>
2015-03-13 06:55:47 -0500

    Merge serverStateHelpers into proper locations
    I was being dumb when I made that. Moved the functions into the states.

--- src/server/serverControllerState.cpp
+++ src/server/serverControllerState.cpp
@@ -1,30 +1,79 @@
 #include "serverControllerState.hpp"
-#include "serverStateHelpers.hpp"
+#include "serverInfo.hpp"
+#include "serverCustomActions.hpp"
 
-ServerControllerState::ServerControllerState( std::string state_type_desc, ServInfo* mod )
-    : ControllerState(state_type_desc), model( mod ) {
-}
+#ifdef _WIN32
+    #include <winsock2.h>
+#else
+    #include <netdb.h>
+#endif
 
-WaitingForClientsState::WaitingForClientsState( ServInfo* mod ) : ServerControllerState( "Voting", mod ) {
-
-}
-
-WaitingForClientsState::~WaitingForClientsState( ) { }
-
-ServerControllerState* WaitingForClientsState::handleAction( Action* action_to_be_handled ) {
-
-    ServerStateHelper helper( model );
-
-    std::string action_type = action_to_be_handled->getMessage();
-
-    if( action_type == "NewPlayer" ) {
-        auto action = dynamic_cast< NewPlayerAction* >( action_to_be_handled );
-        unsigned int playerID = action->getPlayerID( );
-        helper.sendStartingInfo( playerID );
-    } else {
-        reportUnhandledAction( action_type );
+// ServerControllerState {
+    ServerControllerState::ServerControllerState( std::string state_type_desc, ServInfo* mod )
+        : ControllerState(state_type_desc), model( mod ) {
     }
 
-    return NULL;
-}
+    // Sends one player another player's information
+    void ServerControllerState::sendPlayer( int playerID, int destinationID, bool allInfo ) {
 
+        avalon::network::Player playerBuf;
+
+        if ( allInfo ) {
+            playerBuf = model->players[ playerID ]->getBuf( );
+        } else {
+            playerBuf = model->players[ playerID ]->getHiddenBuf( );
+        }
+
+        playerBuf.set_id( playerID );
+        sendProtobuf( avalon::network::PLAYER_BUF, destinationID, playerBuf.SerializeAsString( ) );
+    }
+
+    // Sends a protobuf to a player
+    void ServerControllerState::sendProtobuf( avalon::network::buffers_t bufType, int destinationID, std::string message ) {
+
+        int messageSize = message.length( );
+
+        // Windows REQUIRES that model->sockets send char* rather than void*... so we have to do some bullshit to trick it
+        send( model->sockets[ destinationID ], (char*)(&bufType), sizeof( avalon::network::buffers_t ) / sizeof( char ), 0 );
+        send( model->sockets[ destinationID ], (char*)(&messageSize), sizeof( int ) / sizeof( char ), 0 );
+        send( model->sockets[ destinationID ], message.c_str( ), messageSize * sizeof( char ), 0 );
+    }
+// }
+
+// WaitingForClientsState {
+    WaitingForClientsState::WaitingForClientsState( ServInfo* mod ) : ServerControllerState( "Voting", mod ) {
+
+    }
+
+    WaitingForClientsState::~WaitingForClientsState( ) { }
+
+    ServerControllerState* WaitingForClientsState::handleAction( Action* action_to_be_handled ) {
+
+        std::string action_type = action_to_be_handled->getMessage();
+
+        if( action_type == "NewPlayer" ) {
+            auto action = dynamic_cast< NewPlayerAction* >( action_to_be_handled );
+            unsigned int playerID = action->getPlayerID( );
+            sendStartingInfo( playerID );
+        } else {
+            reportUnhandledAction( action_type );
+        }
+
+        return NULL;
+    }
+
+
+    // Sends the beginning information when a player connects
+    void WaitingForClientsState::sendStartingInfo( int playerID ) {
+
+        model->settingsBuf.set_client( playerID );
+        sendProtobuf( avalon::network::SETTINGS_BUF, playerID, model->settingsBuf.SerializeAsString( ) );
+
+        for( int i = 0; i < playerID; i++ ) {
+            sendPlayer( i, playerID, false ); // Send each currently connected player to the new player
+            sendPlayer( playerID, i, false ); // Send the new player to each player already connected
+        }
+
+        sendPlayer( playerID, playerID, true ); // Send the new player their own info
+    }
+// }
--- src/server/serverControllerState.hpp
+++ src/server/serverControllerState.hpp
@@ -1,3 +1,12 @@
+/**
+ * Main class for the server states
+ * Individual states inherit from this class
+ *
+ * @class ServerStateHelpers
+ * @author Ryan kerr
+ * @date 2015-03-12
+ */
+
 #ifndef SERVER_CONTROLLER_STATE_HPP
 #define SERVER_CONTROLLER_STATE_HPP
 
@@ -6,23 +15,55 @@
 
 #include "action.hpp"
 #include "serverInfo.hpp"
-#include "serverCustomActions.hpp"
 #include "controllerState.hpp"
 
 class ServerControllerState : public ControllerState {
-public:
-    ServerControllerState( std::string state_type_desc, ServInfo* mod );
 
-protected:
-    ServInfo* model;
+    public:
+        ServerControllerState( std::string state_type_desc, ServInfo* mod );
+
+    protected:
+        ServInfo* model;
+
+        /**
+          * Helper function to send a player to another player
+          *
+          * @param playerID The ID of the player to be sent
+          * @param destintationID The ID of the player to send the player to
+          * @param allInfo Whether the player should know the role and alignment of the sent player
+          * @return None
+          */
+        void sendPlayer( int playerID, int destinationID, bool allInfo );
+
+        /**
+          * Helper function to send a protobuf
+          *
+          * @param bufType The type of the buffer that is being sent
+          * @param destinationID The ID of the player that the buffer should be sent to
+          * @param message The protobuf to be sent, already serialized as a string (Using protobuf's method)
+           * @return None
+         */
+        void sendProtobuf( avalon::network::buffers_t bufType, int destinationID, std::string message );
 };
 
 class WaitingForClientsState : public ServerControllerState {
-public:
-    WaitingForClientsState( ServInfo* mod );
-    ~WaitingForClientsState( );
 
-    ServerControllerState* handleAction( Action* action_to_be_handled );
+    public:
+        WaitingForClientsState( ServInfo* mod );
+        ~WaitingForClientsState( );
+
+        ServerControllerState* handleAction( Action* action_to_be_handled );
+
+    private:
+        /**
+          * Helper function to send everything a player needs upon connecting to the server
+          * Sends settings, their player object, and all currently connected players
+          * Also sends the new player to all currently connected players
+          *
+          * @param playerID the ID of the player that is connecting
+          * @return None
+          */
+        void sendStartingInfo( int playerID );
 };
 
 #endif // SERVER_CONTROLLER_STATE_HPP
