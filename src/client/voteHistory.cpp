6498303695c3537ca5576e97cabbcb517e94ecac
Mohammad Askari <mhaskari92@gmail.com>
2015-03-18 17:18:23 -0500

    Create voteHistory.cpp

---
+++ src/client/voteHistory.cpp
@@ -0,0 +1,36 @@
+/*
+ * Implementation of VoteHistory class
+ *
+ * @file voteHistory.cpp
+ * @author Mohammad Askari && Matt Hoffman
+ * @date 2015-03-18
+*/
+
+#include "voteHistory.hpp"
+
+VoteHistory::VoteHistory( std::vector< avalon::player_vote_t > playerVotesVec, std::vector< unsigned int > teamVotedOn, unsigned int voteTrackDuringVote, unsigned int questTrackDuringVote ) {
+    playerVotes = playerVotesVec;
+    proposedTeam = teamVotedOn;
+    voteTrackNum = voteTrackDuringVote;
+    questTrackNum = questTrackDuringVote;
+}
+
+avalon::player_vote_t getPlayerVote ( unsigned int playerNum ) {
+    return playerVotes.at(playerNum);
+}
+
+std::vector< avalon::player_vote_t > getPlayerVotes( ) {
+    return playerVotes;
+}
+
+std::vector< unsigned int > getProposedTeam( ) {
+    return proposedTeam;
+}
+
+unsigned int getVoteTrackNum( ) {
+    return voteTrackNum;
+}
+
+unsigned int getQuestTrackNum( ) {
+    return questTrackNum;
+}
