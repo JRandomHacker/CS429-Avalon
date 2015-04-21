f3d1cc62f476d6dcf0ea32c8d71a76b910769872
Ryan Kerr <rkerr2@illinois.edu>
2015-04-20 22:33:16 -0500

    Small bug fix of typing in the model
    We called something a vector of player pointers when it was actually
    player objects

--- src/client/lobbyState.cpp
+++ src/client/lobbyState.cpp
@@ -50,7 +50,7 @@ namespace client {
             data->model->addData< unsigned int >( "assassinTargeted", 0 );
             data->model->addData( "endGameState", false );
             data->model->addData< avalon::alignment_t >( "winningTeam", avalon::UNKNOWN_ALIGN ); 
-            data->model->addData( "endGamePlayers", std::vector< Player* >( ) );
+            data->model->addData( "endGamePlayers", std::vector< Player >( ) );
             data->model->addData( "chatMessages", std::vector< avalon::common::ChatMessage >( ) );
             
             std::vector< unsigned int > players_per_quest;
