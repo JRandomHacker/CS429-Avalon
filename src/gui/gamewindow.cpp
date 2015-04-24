cbb8d5318366f6bc086814e86dab6e3780414e59
Daniel Simpson <dcsimps2@illinois.edu>
2015-04-23 23:31:42 -0500

    Updates to endgame layout. Got rid of a bunch of QStrings that I discovered werent actually necessary!

--- src/gui/gamewindow.cpp
+++ src/gui/gamewindow.cpp
@@ -114,7 +114,7 @@ void GameWindow::createPlayerSubscribersSlot( ) {
 void GameWindow::createPlayerSubscribers( ) {
 
     ui->votingSection->hide( );
-    ui->stateLabel->setText( QString( "Team Select" ) );
+    ui->stateLabel->setText( "Team Select" );
 
     // Add subscriber for number of players
     num_players_subscriber = new ClosureSubscriber( NULL, NULL );
@@ -182,7 +182,7 @@ void GameWindow::createPlayerSubscribers( ) {
 
         voteTrackLabels[i] = new QLabel("");
         voteTrackLabels[i]->setPixmap( QPixmap( ":/images/QUEST_UNKNOWN.png" ) );
-        voteTrackLabels[i]->setToolTip( QString("No quest history to display.") );
+        voteTrackLabels[i]->setToolTip( "No quest history to display." );
 
         ui->votingTrackLayout->addWidget(voteTrackLabels[i]);
     }
@@ -196,9 +196,9 @@ void GameWindow::createPlayerSubscribers( ) {
     ui->playerList->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 
     QStringList headers;
-    headers.append( QString( "Name" ) );
-    headers.append( QString( "Alignment" ) );
-    headers.append( QString( "Role" ) );
+    headers.append( "Name" );
+    headers.append( "Alignment" );
+    headers.append( "Role" );
 
     listModel->setHorizontalHeaderLabels( headers );
 
@@ -322,7 +322,7 @@ void GameWindow::updatePlayer( unsigned int id ) {
             //Load client player's role avatar
             ui->playerAvatarLabel->setVisible(false);
             ui->playerAvatarLabel->setPixmap( QPixmap( avalon::gui::roleToImage( p->getRole(), p->getAlignment() ).c_str( ) ) );
-            ui->playerAvatarLabel->setToolTip( QString( avalon::gui::roleToFlavorText( p->getRole(), p->getAlignment() ).c_str( ) ) );
+            ui->playerAvatarLabel->setToolTip( avalon::gui::roleToFlavorText( p->getRole(), p->getAlignment() ).c_str( ) );
             ui->playerAvatarLabel->setVisible(true);
 
             if( p->getRole( ) == avalon::ASSASSIN ) {
@@ -334,12 +334,12 @@ void GameWindow::updatePlayer( unsigned int id ) {
             newString += " (host)";
 
         listModel->setItem( id, 1, new QStandardItem(
-                                QString( avalon::gui::alignmentToString( p->getAlignment( ) ).c_str( ) ) ) );
+                                avalon::gui::alignmentToString( p->getAlignment( ) ).c_str( ) ) );
 
         listModel->setItem( id, 2, new QStandardItem(
-                                QString( avalon::gui::roleToString( p->getRole( ) ).c_str( ) ) ) );
+                                avalon::gui::roleToString( p->getRole( ) ).c_str( ) ) );
     }
-    listModel->setItem( id, 0, new QStandardItem( QString( newString.c_str( ) ) ) );
+    listModel->setItem( id, 0, new QStandardItem( newString.c_str( ) ) );
     
     ui->playerList->resizeColumnsToContents( );
 }
@@ -356,19 +356,19 @@ void GameWindow::updateLeader( ) {
 
     if( lid == myID ) {
 
-        ui->leaderLabel->setText( QString( "You are the leader!" ) );
+        ui->leaderLabel->setText( "You are the leader!" );
         ui->proposeTeamButton->show( );
         ui->proposeTeamButton->setEnabled( false );
     } else if( lid != UINT_MAX ) {
 
         Player* leader = player_subscribers[lid]->getData<Player>( );
         if( leader != NULL ) {
-            ui->leaderLabel->setText( QString( ( "Current Leader: "+leader->getName( ) ).c_str( ) ) );
+            ui->leaderLabel->setText( ( "Current Leader: "+leader->getName( ) ).c_str( ) );
         }
         ui->proposeTeamButton->hide( );
     } else {
 
-        ui->leaderLabel->setText( QString( "" ) );
+        ui->leaderLabel->setText( "" );
         ui->proposeTeamButton->hide( );
     }
 }
@@ -386,10 +386,10 @@ void GameWindow::updateQuestingTeam( ) {
 
         unsigned int player_num = team[ i ];
         Player* p = player_subscribers[ player_num ]->getData< Player >( );
-        qModel->appendRow( new QStandardItem( QString( p->getName( ).c_str( ) ) ) );
+        qModel->appendRow( new QStandardItem( p->getName( ).c_str( ) ) );
 
         QStringList qHeaders;
-        qHeaders.append( QString( "Name" ) );
+        qHeaders.append( "Name" );
         qModel->setHorizontalHeaderLabels( qHeaders );
     }
 
@@ -403,8 +403,8 @@ void GameWindow::updateQuestingTeam( ) {
     std::vector<unsigned int> playersPerQuest = *playersPerQuest_subscriber->getData<std::vector<unsigned int>>( );
     unsigned int currQuest = *currentQuestTrack_subscriber->getData<unsigned int>( );
     unsigned int playersCurrQuest = playersPerQuest[currQuest];
-    ui->teamSizeLabel->setText( QString( (std::to_string( team.size( ) ) + "/" + std::to_string( playersCurrQuest )
-                                         + " players selected").c_str( ) ) );
+    ui->teamSizeLabel->setText( (std::to_string( team.size( ) ) + "/" + std::to_string( playersCurrQuest )
+                                         + " players selected").c_str( ) );
 
     ui->proposeTeamButton->setEnabled( playersCurrQuest == team.size( ) );
 }
@@ -419,11 +419,11 @@ void GameWindow::updateGameInfo( ) {
 
     std::string num_evil_string = "Evil Players: " + std::to_string( num_evil );
 
-    ui->numOfEvilPlayers->setText( QString( num_evil_string.c_str( ) ) );
+    ui->numOfEvilPlayers->setText( num_evil_string.c_str( ) );
 
     std::vector< avalon::special_roles_t >* roleList = roleList_subscriber->getData< std::vector< avalon::special_roles_t > >( );
     if( roleList != NULL ) {
-        QString roles = QString( "Roles: " );
+        QString roles = "Roles: ";
         for( unsigned int i = 0; i < roleList->size( ); i++ ) {
             avalon::special_roles_t role = ( *roleList )[ i ];
             if( i != 0 ) {
@@ -535,7 +535,7 @@ void GameWindow::updateTrack( ) {
         else
             voteTrackLabels[currQuest-1]->setPixmap( QPixmap( ":/images/QUEST_FAIL.png" ) );
 
-        voteTrackLabels[currQuest-1]->setToolTip( QString(buildQuestHistoryString( ).c_str( )) );
+        voteTrackLabels[currQuest-1]->setToolTip( buildQuestHistoryString( ).c_str( ) );
 
     }
 
@@ -553,7 +553,7 @@ void GameWindow::updateTeamVoteState( ) {
     QStandardItemModel* listModel = (QStandardItemModel*) ui->playerList->model( );
     if( inVoteState ) {
 
-        ui->stateLabel->setText( QString( "Team Vote" ) );
+        ui->stateLabel->setText( "Team Vote" );
 
         // Hide the propose team button
         ui->proposeTeamButton->hide( );
@@ -562,7 +562,7 @@ void GameWindow::updateTeamVoteState( ) {
         for( int i = 0; i < listModel->rowCount( ); i++ ) {
             listModel->setItem( i, 3, new QStandardItem( "No" ) );
         }
-        listModel->setHorizontalHeaderItem( 3, new QStandardItem( QString( "Has Voted" ) ) );
+        listModel->setHorizontalHeaderItem( 3, new QStandardItem( "Has Voted" ) );
         ui->votingSection->show( );
 
         // Subscribe to the data
@@ -589,13 +589,13 @@ void GameWindow::updateQuestVoteState( ) {
     bool inQuestVote = *questVoteState_subscriber->getData<bool>( );
     QStandardItemModel* listModel = (QStandardItemModel*) ui->proposeTeamList->model( );
     if( inQuestVote ) {
-        ui->stateLabel->setText( QString( "Quest Vote" ) );
+        ui->stateLabel->setText( "Quest Vote" );
 
         // Create the "Has Voted" column
         for( int i = 0; i < listModel->rowCount( ); i++ ) {
             listModel->setItem( i, 1, new QStandardItem( "No" ) );
         }
-        listModel->setHorizontalHeaderItem( 1, new QStandardItem( QString( "Has Voted" ) ) );
+        listModel->setHorizontalHeaderItem( 1, new QStandardItem( "Has Voted" ) );
 
         // Display vote buttons if on team
         std::vector<unsigned int> team = *questingTeam_subscriber->getData<std::vector<unsigned int>>( );
@@ -631,7 +631,7 @@ void GameWindow::updateQuestVoteState( ) {
         model->subscribe( "currentVotes", currentVotes_subscriber );
     } else {
         listModel->removeColumn( 1 );
-        ui->stateLabel->setText( QString( "Team Selection" ) );
+        ui->stateLabel->setText( "Team Selection" );
         ui->votingSection->hide( );
         ui->buttonVoteFail->setText( "FAIL!!!" );
     }
@@ -648,22 +648,23 @@ void GameWindow::updateAssassinState( ) {
 
     bool inAssassinState = *assassinState_subscriber->getData<bool>( );
     if( inAssassinState ) {
-        ui->stateLabel->setText( QString( "Assassin Round" ) );
+        ui->stateLabel->setText( "Assassin Round" );
         ui->teamSizeLabel->hide( );
         ui->votingSection->hide( );
+        ui->proposedTeamSection->setTitle( "Assassination" );
 
         assassinChoice = -1;
 
         if( amAssassin ) {
-            ui->leaderLabel->setText( QString( "You are the assassin!  Choose a player to kill." ) );
-            ui->proposeTeamButton->setText( QString( "Assassinate" ) );
+            ui->leaderLabel->setText( "You are the assassin!  Choose a player to kill." );
+            ui->proposeTeamButton->setText( "Assassinate" );
             ui->proposeTeamButton->show( );
             ui->proposeTeamButton->setEnabled( false );
 
             ui->proposeTeamList->setModel( new QStandardItemModel( 0 ) );
 
         } else {
-            ui->leaderLabel->setText( QString( "An assassin walks among you..." ) );
+            ui->leaderLabel->setText( "An assassin walks among you..." );
             ui->proposeTeamList->hide( );
             ui->proposeTeamButton->hide( );
         }
@@ -680,7 +681,7 @@ void GameWindow::updateEndGameState( ) {
     bool inEndGameState = *endGameState_subscriber->getData<bool>( );
     if( inEndGameState ) {
 
-        ui->stateLabel->setText( QString( "Game Over" ) );
+        ui->stateLabel->setText( "Game Over" );
 
         // Get pertinent info: winning team, my player obj, my pID, assassin stuff
         Subscriber* winningTeam_subscriber = new ClosureSubscriber( NULL, NULL );
@@ -695,6 +696,12 @@ void GameWindow::updateEndGameState( ) {
 
         // Display the result of the game
         std::string gameResultString = avalon::gui::getGameResultString( myPlayer.getAlignment( ), winner );
+
+        ui->proposeTeamButton->hide( );
+        ui->proposeTeamList->hide( );
+        ui->teamSizeLabel->hide( );
+        ui->proposedTeamSection->setTitle( "Results" );
+        ui->leaderLabel->setText( gameResultString.c_str( ) );
 
     }
 }
@@ -732,7 +739,7 @@ void GameWindow::updateCurrentVotes( ) {
         for( unsigned int i = 0; i < votes.size( ); i++ ) {
             if( votes[i] != avalon::NO_VOTE ) {
                 QStandardItemModel* listModel = ( QStandardItemModel* ) ui->playerList->model( );
-                listModel->item( i, 3 )->setText( QString( "Yes" ) );
+                listModel->item( i, 3 )->setText( "Yes" );
             }
         }
     } else {
@@ -747,7 +754,7 @@ void GameWindow::updateCurrentVotes( ) {
                     }
                 }
                 QStandardItemModel* listModel = ( QStandardItemModel* ) ui->proposeTeamList->model( );
-                listModel->item( listPos, 1 )->setText( QString( "Yes" ) );
+                listModel->item( listPos, 1 )->setText( "Yes" );
             }
         }
     }
@@ -802,7 +809,7 @@ void GameWindow::updateChatMessages( ) {
                                   std::not1(std::ptr_fun<int, int>(std::isspace))).base(), messageStr.end());
 
     QStandardItemModel* listModel = (QStandardItemModel*) ui->chatList->model( );
-    QStandardItem* messageItem = new QStandardItem( QString( messageStr.c_str( ) ) );
+    QStandardItem* messageItem = new QStandardItem( messageStr.c_str( ) );
     listModel->setItem( listModel->rowCount( ), messageItem );
     ui->chatList->scrollToBottom( );
 }
@@ -818,7 +825,7 @@ void GameWindow::on_playerList_clicked( const QModelIndex& index ) {
     if( *assassinState_subscriber->getData<bool>( ) && amAssassin ) {
         assassinChoice = row;
         std::string chosenName = player_subscribers[row]->getData<Player>( )->getName( );
-        QStandardItem* choiceItem = new QStandardItem( QString( chosenName.c_str( ) ) );
+        QStandardItem* choiceItem = new QStandardItem( chosenName.c_str( ) );
         QStandardItemModel* newModel = new QStandardItemModel( );
         newModel->setItem( 0, choiceItem );
         ui->proposeTeamList->setModel( newModel );
