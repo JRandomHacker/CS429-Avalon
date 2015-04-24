596c88b17cd4adf5aa2a9094695c678e9ffde2ec
Durendal35 <durendal35@gmail.com>
2015-04-23 19:47:00 -0500

    Added help text when hovering over character port
    Added semi-useful help text that appears whenever you hover your cursor
    over your character portrait. Not in the Use Cases but a very simple
    implementation.

--- src/gui/guihelpers.cpp
+++ src/gui/guihelpers.cpp
@@ -57,6 +57,34 @@ std::string avalon::gui::roleToImage( avalon::special_roles_t role, avalon::alig
             return desire += "NONE.jpg";
 }
 
+std::string avalon::gui::roleToFlavorText(avalon::special_roles_t role, avalon::alignment_t align) {
+    switch( role ){
+        case avalon::MERLIN:
+            return "You are Merlin, the Great Wizard. You know who is evil, and must stay hidden.";
+        case avalon::MORDRED:
+            return "You are Mordred, Master of Evil. Your presence is unknown to Merlin.";
+        case avalon::MORGANA:
+            return "You are Morgana, the Deciever. You appear as Merlin.";
+        case avalon::ASSASSIN:
+            return "You are Assassin, Agent of Evil. You have an extra chance to eliminate Merlin.";
+        case avalon::OBERON:
+            return "You are Oberon, a Strange Man. Your presence is unknown to Evil.";
+        case avalon::PERCIVAL:
+            return "You are Percival, Guardian of Good. You know who Merlin is.";
+        case avalon::NONE:
+            break;
+        case avalon::UNKNOWN_ROLE:
+            break;
+    }
+
+    if(role == avalon::NONE && align == avalon::GOOD)
+            return "You are a Loyal Servant of Arthur.";
+    else if(role == avalon::NONE && align == avalon::EVIL)
+            return "You are a Minion of Mordred.";
+    else
+            return "You are Hodor, One Who Does Not Exist. This role should not exist.";
+}
+
 std::string avalon::gui::alignmentToString( avalon::alignment_t align ) {
     switch( align ) {
 
